55be121f6e0600c9c0bfd04868df9cd922cc1b86
Nasilnov <nasilnov@mail.ru>
2021-02-28 15:25:47 +0300

    lesson3

--- app/src/main/java/ru/tikhvin/city/android/androidl1/CalcActivity.java
+++ app/src/main/java/ru/tikhvin/city/android/androidl1/CalcActivity.java
@@ -1,14 +1,10 @@
 package ru.tikhvin.city.android.androidl1;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,23 +12,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class CalcActivity extends AppCompatActivity implements View.OnClickListener {
-    ArrayList<View> allButtons;
-    protected TextView result;
+
+    public static final String CALC_PREFIX = CalcActivity.class.getCanonicalName();
+    public static final String CALC_ARG1 = CALC_PREFIX + ".AGR1";
+    public static final String CALC_ARG2 = CALC_PREFIX + ".AGR2";
+    public static final String CALC_OPERATOR = CALC_PREFIX + ".OPERATOR";
+    public static final String CALC_FLAG = CALC_PREFIX + ".FLAG";
+    public static final String CALC_RESULT = CALC_PREFIX + ".RESULT";
+
+    ArrayList<View> mAllButtons;
+    protected TextView mResult;
+    private  String mArg1 = "";
+    private  String mArg2 = "";
+    private  String mOperator = "";
+    private int mFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.keyboard_linear);
-        allButtons = ((LinearLayout) findViewById(R.id.button_container)).getTouchables();
-        for (int i = 0; i < allButtons.size(); i++) {
-            allButtons.get(i).setOnClickListener(this);
+        mAllButtons = ((LinearLayout) findViewById(R.id.button_container)).getTouchables();
+        for (int i = 0; i < mAllButtons.size(); i++) {
+            mAllButtons.get(i).setOnClickListener(this);
         }
-        result = findViewById(R.id.result);
+        mResult = findViewById(R.id.result);
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putString(CALC_ARG1, mArg1);
+        state.putString(CALC_ARG2, mArg2);
+        state.putString(CALC_OPERATOR, mOperator);
+        state.putString(CALC_RESULT, (String) mResult.getText());
+        state.putInt(CALC_FLAG, mFlag);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        mArg1       = state.getString(CALC_ARG1);
+        mArg2       = state.getString(CALC_ARG2);
+        mOperator   = state.getString(CALC_OPERATOR);
+        mFlag       = state.getInt(CALC_FLAG);
+        mResult.setText(state.getString(CALC_RESULT));
+    }
+
+
+    @Override
     public void onClick(View v) {
-        Button b;
+        Button b = (Button) v;
         switch (v.getId()) {
             case R.id.button0:
             case R.id.button1:
@@ -44,39 +73,124 @@ public class CalcActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button7:
             case R.id.button8:
             case R.id.button9:
+                if (mFlag == 1) {
+                    mResult.setText("");
+                }
+                mFlag = 0;
+                mResult.setText(mResult.getText() + b.getText().toString());
+                break;
             case R.id.buttonPt:
-                b = (Button) v;
-                result.setText(result.getText() + b.getText().toString());
+                if (!checkPoint((String) mResult.getText())) {
+                    mResult.setText(mResult.getText() + b.getText().toString());
+                }
+                break;
+            case R.id.button_clear:
+                clear();
+                break;
+
+            case R.id.button_back:
+                mResult.setText(backSpace((String) mResult.getText()));
+                break;
+            case R.id.button_plus:
+            case R.id.button_minus:
+            case R.id.button_div:
+            case R.id.button_mult:
+                setOperation((String) b.getText());
+                break;
+            case R.id.button_proc:
+                if(!mArg1.equals("") && !mArg1.equals("")) {
+                    mArg2 =  Float.toString(Float.parseFloat((String) mResult.getText()) * Float.parseFloat(mArg1) / 100 );
+                    mResult.setText(numberAction());
+                    mArg1 = "";
+                    mArg2 = "";
+                    mOperator = "";
+                }
+                break;
+            case R.id.button_equal:
+                if(!mArg1.equals("") && !mArg1.equals("")) {
+                    mArg2 = (String) mResult.getText();
+                    mResult.setText(numberAction());
+                    mArg1 = "";
+                    mArg2 = "";
+                    mOperator = "";
+                }
                 break;
             default: break;
         }
 
     }
 
+    private void clear() {
+        mResult.setText("");
+        mArg1 ="";
+        mArg2 = "";
+        mOperator = "";
+    }
 
-//    private String numberAction( EditText a, EditText b, String c , CheckBox r) {
-//        float d = 0;
-//        try {
-//            arg1 = Integer.valueOf(a.getText().toString());
-//            arg2 = Integer.valueOf(b.getText().toString());
-//        } catch (NumberFormatException e) {
-//            e.printStackTrace();
-//            return "Enter a number";
-//        }
-//
-//        switch (c) {
-//            case "+": d = (float) arg1 + arg2;
-//                break;
-//            case "-": d = (float) arg1 - arg2;
-//                break;
-//            case "*": d = (float) arg1 * arg2;
-//                break;
-//            case "/": d = (float) arg1 / arg2;
-//                break;
-//            default: return  "Что-то пошло не так";
-//        }
-//
-//        return Float.toString(d);
-//    }
+    private void setOperation(String operator) {
+
+        if ( !mArg1.equals("") && mArg2.equals("")) {
+            mArg2 = (String) mResult.getText();
+            mResult.setText(numberAction());
+            mArg1 = numberAction();
+            mArg2 = "";
+            mOperator = operator;
+            mFlag = 1;
+            return;
+        }
+
+        if ( mArg1.equals("") ) {
+            mArg1 = (String) mResult.getText();
+            mOperator = operator;
+            mResult.setText("");
+            return;
+        }
+
+    }
+
+
+    private boolean checkPoint(String str) {
+         if (str.contains(".")) {
+             return true;
+         }
+         else return false;
+    }
+
+    private String backSpace (String str) {
+        if (str.length() > 0 ) {
+            return str.substring(0, str.length() - 1);
+        } else return "";
+    }
+
+
+    private String numberAction() {
+        float arg1;
+        float arg2;
+        float res = 0;
+
+        try {
+            arg1 = Float.valueOf(mArg1);
+            arg2 = Float.valueOf(mArg2);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            mArg1 = "";
+            mArg2 = "";
+            mOperator = "";
+            return "Error";
+        }
+
+        switch (mOperator) {
+            case "+": res = arg1 + arg2;
+                break;
+            case "-": res = arg1 - arg2;
+                break;
+            case "*": res =  arg1 * arg2;
+                break;
+            case "/": res = arg1 / arg2;
+                break;
+        }
+
+        return Float.toString(res);
+    }
 
 }
